e843a7e118e8836a80eaa8d9789989e4331213e7
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-03-27 18:34:57 +0800

    cleanup

--- src/Properties.php
+++ src/Properties.php
@@ -4,105 +4,104 @@
 
 	abstract class Properties
 	{
-		private static array $post = [];
-		private static array $json = [];
-		private static array $server = [];
-		private static array $files = [];
-		private static array $cookies = [];
-		private static array $headers = [];
-		private static array $query = [];
-		private static array $raw_input = [];
-		private static array $xml = [];
-		private static array $session = [];
+		private static array $data = [
+			'post' => [],
+			'json' => [],
+			'server' => [],
+			'files' => [],
+			'cookies' => [],
+			'headers' => [],
+			'query' => [],
+			'raw_input' => [],
+			'xml' => [],
+			'session' => []
+		];
 
 		public static function capture(): void
 		{
-			$dataResources = [
+			self::processSuperGlobals();
+			self::processRawInput();
+			self::processFiles();
+		}
+
+		private static function processSuperGlobals(): void
+		{
+			$sources = [
 				'query' => $_GET,
 				'post' => $_POST,
 				'server' => $_SERVER,
-				'cookie' => $_COOKIE,
+				'cookies' => $_COOKIE,
 				'headers' => getallheaders(),
 				'session' => $_SESSION ?? []
 			];
 
-			foreach ($dataResources as $type => $data) {
+			foreach ($sources as $type => $data) {
 				foreach ($data as $key => $value) {
-					self::register($key, $value, $type);
+					self::register($type, $key, $value);
 				}
 			}
+		}
 
+		private static function processRawInput(): void
+		{
 			$rawInput = file_get_contents('php://input');
-			if ($rawInput) {
-				if (str_contains($rawInput, '<') && $xmlData = simplexml_load_string($rawInput)) {
-					foreach ($xmlData as $key => $value) {
-						self::register($key, (string)$value, 'xml');
-					}
-				} elseif (preg_match('/^[a-zA-Z0-9\s]*$/', $rawInput)) {
-					self::register('plain_text', $rawInput, 'raw_input');
-				} else {
-					self::register('raw_input', $rawInput, 'raw_input');
+			if (!$rawInput) return;
+
+			if (str_contains($rawInput, '<') && ($xmlData = simplexml_load_string($rawInput))) {
+				foreach ($xmlData as $key => $value) {
+					self::register('xml', $key, (string)$value);
 				}
+			} elseif (preg_match('/^[a-zA-Z0-9\s]*$/', $rawInput)) {
+				self::register('raw_input', 'plain_text', $rawInput);
+			} else {
+				self::register('raw_input', 'raw_input', $rawInput);
 			}
 
-			$jsonPayload = json_decode($rawInput, true);
-			if ($jsonPayload) {
+			if ($jsonPayload = json_decode($rawInput, true)) {
 				foreach ($jsonPayload as $key => $value) {
-					self::register($key, $value, 'json');
+					self::register('json', $key, $value);
 				}
 			}
+		}
 
-			$indexedFiles = [];
+		private static function processFiles(): void
+		{
 			foreach ($_FILES as $key => $attr) {
-
-				if (isset($attr[ 'name' ]) && is_array($attr[ 'name' ])) {
-					if (!isset($indexedFiles[$key]))
-						$indexedFiles[$key] = [];
-
-					foreach ($attr[ 'name' ] as $index => $name) {
-						if (trim($name ?? '')) {
-							$indexedFiles[$key][] = [
-								'name' => $name,
-								'full_path' => $attr[ 'full_path' ][ $index ],
-								'type' => $attr[ 'type' ][ $index ],
-								'tmp_name' => $attr[ 'tmp_name' ][ $index ],
-								'error' => $attr[ 'error' ][ $index ],
-								'size' => $attr[ 'size' ][ $index ]
-							];
+				if (isset($attr['name']) && is_array($attr['name'])) {
+					$files = [];
+					foreach ($attr['name'] as $index => $name) {
+						if (trim($name)) {
+							$files[] = self::formatFileAttributes($attr, $index);
 						}
 					}
-				} else {
-					if (!empty($attr[ 'name' ])) {
-						$indexedFiles[$key] = [
-							'name' => $attr[ 'name' ],
-							'full_path' => $attr[ 'full_path' ],
-							'type' => $attr[ 'type' ],
-							'tmp_name' => $attr[ 'tmp_name' ],
-							'error' => $attr[ 'error' ],
-							'size' => $attr[ 'size' ]
-						];
-					}
+					self::register('files', $key, $files);
+				} elseif (!empty($attr['name'])) {
+					self::register('files', $key, self::formatFileAttributes($attr));
 				}
 			}
+		}
 
-			foreach ($indexedFiles as $file => $files) {
-				self::register($file, $files, 'files');
-			}
+		private static function formatFileAttributes(array $attr, ?int $index = null): array
+		{
+			return [
+				'name' => $index !== null ? $attr['name'][$index] : $attr['name'],
+				'full_path' => $index !== null ? $attr['full_path'][$index] : $attr['full_path'],
+				'type' => $index !== null ? $attr['type'][$index] : $attr['type'],
+				'tmp_name' => $index !== null ? $attr['tmp_name'][$index] : $attr['tmp_name'],
+				'error' => $index !== null ? $attr['error'][$index] : $attr['error'],
+				'size' => $index !== null ? $attr['size'][$index] : $attr['size']
+			];
 		}
 
 		protected static function fetch(string $property): array
 		{
-			if (!isset(self::$$property))
-				return [];
-
-			return self::$$property;
+			return self::$data[$property] ?? [];
 		}
 
-		protected static function register(string $key, mixed $value, string $property): void
+		protected static function register(string $property, string $key, mixed $value): void
 		{
-			$property = strtolower($property);
-			if (isset(self::$$property)) {
-				self::$$property[$key] = $value;
+			if (isset(self::$data[$property])) {
+				self::$data[$property][$key] = $value;
 			}
 		}
 	}
